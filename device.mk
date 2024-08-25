61c8dbf0375f217676a443b3e5216b31cd094230
Michael Bestas <mkbestas@lineageos.org>
2024-08-25 18:52:18 +0300

    veux: Shim libwvhidl and libwvdrmengine with libcrypto_shim
    The following change
    https://github.com/google/boringssl/commit/e202e51cb0912f36dafbd2e67cf04d6ec82f3180
    removed symbols that our libs require.
    
    Change-Id: I1325c34ecec3d339dd68c665010ecb36fa2e6a29
    Signed-off-by: Frost <159105703+Karan-Frost@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -150,7 +150,8 @@ PRODUCT_PACKAGES += \
     android.hardware.drm-service.clearkey
 
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.3.vendor
+    android.hardware.drm@1.3.vendor \
+    libcrypto_shim
 
 # Fastbootd
 PRODUCT_PACKAGES += \
